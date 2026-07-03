6c6db072736fb0c3e705c8111fac1686862c4fe2
Borys35 <boryskac10@gmail.com>
2026-07-03 17:45:38 +0200

    add cubie multiply method

--- include/cubie_cube.hpp
+++ include/cubie_cube.hpp
@@ -30,5 +30,5 @@ public:
         }
     }
 
-    void multiply();
+    void multiply(const CubieCube &b);
 };
--- src/cubie_cube.cpp
+++ src/cubie_cube.cpp
@@ -1,5 +1,20 @@
 #include "cubie_cube.hpp"
 
-void CubieCube::multiply()
+void CubieCube::multiply(const CubieCube &b)
 {
+    CubieCube result;
+
+    for (int i = 0; i < CORNER_COUNT; ++i)
+    {
+        result.cp[i] = cp[static_cast<int>(b.cp[i])];
+        result.co[i] = (co[static_cast<int>(b.cp[i])] + b.co[i]) % 3;
+    }
+
+    for (int i = 0; i < EDGE_COUNT; ++i)
+    {
+        result.ep[i] = ep[static_cast<int>(b.ep[i])];
+        result.eo[i] = (eo[static_cast<int>(b.ep[i])] + b.eo[i]) % 2;
+    }
+
+    *this = result;
 }
