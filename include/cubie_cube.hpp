f8c92918b152b75f3a378bed8c6d8f35f303b44e
Borys35 <boryskac10@gmail.com>
2026-08-18 22:30:54 +0200

    add CubieCube::from_facelet_cube and CubieCube::is_solvable

--- include/cubie_cube.hpp
+++ include/cubie_cube.hpp
@@ -56,6 +56,61 @@ public:
     int get_ep8_coordinate() const;
     int get_ud_slice_perm_coordinate() const;
 
+    bool is_solvable() const;
+    void from_facelet_cube(const FaceletCube& fc);
+
 private:
     int cnk(int n, int k) const; // combinatorial number system n choose k
+
+    static constexpr int CORNER_FACELETS[8][3] = {
+        {8, 9, 20},   // URF slot: U, R, F facelets
+        {6, 18, 38},  // UFL slot: U, F, L facelets
+        {0, 36, 47},  // ULB slot: U, L, B facelets
+        {2, 45, 11},  // UBR slot: U, B, R facelets
+        {29, 26, 15}, // DFR slot: D, F, R facelets
+        {27, 44, 24}, // DFL slot: D, L, F facelets
+        {33, 53, 42}, // DLB slot: D, B, L facelets
+        {35, 17, 51}  // DBR slot: D, R, B facelets
+    };
+
+    static constexpr Color CORNER_COLORS[8][3] = {
+        {U_COLOR, R_COLOR, F_COLOR}, // URF piece
+        {U_COLOR, F_COLOR, L_COLOR}, // UFL piece
+        {U_COLOR, L_COLOR, B_COLOR}, // ULB piece
+        {U_COLOR, B_COLOR, R_COLOR}, // UBR piece
+        {D_COLOR, F_COLOR, R_COLOR}, // DFR piece
+        {D_COLOR, L_COLOR, F_COLOR}, // DFL piece
+        {D_COLOR, B_COLOR, L_COLOR}, // DLB piece
+        {D_COLOR, R_COLOR, B_COLOR}  // DBR piece
+    };
+
+    static constexpr int EDGE_FACELETS[12][2] = {
+        {5, 10},  // UR slot: U6, R2
+        {7, 19},  // UF slot: U8, F2
+        {3, 37},  // UL slot: U4, L2
+        {1, 46},  // UB slot: U2, B2
+        {32, 16}, // DR slot: D6, R8
+        {28, 25}, // DF slot: D2, F8
+        {30, 43}, // DL slot: D4, L8
+        {34, 52}, // DB slot: D8, B8
+        {23, 12}, // FR slot: F6, R4
+        {21, 41}, // FL slot: F4, L6
+        {50, 39}, // BL slot: B6, L4
+        {48, 14}  // BR slot: B4, R6
+    };
+
+    static constexpr Color EDGE_COLORS[12][2] = {
+        {U_COLOR, R_COLOR}, // UR piece
+        {U_COLOR, F_COLOR}, // UF piece
+        {U_COLOR, L_COLOR}, // UL piece
+        {U_COLOR, B_COLOR}, // UB piece
+        {D_COLOR, R_COLOR}, // DR piece
+        {D_COLOR, F_COLOR}, // DF piece
+        {D_COLOR, L_COLOR}, // DL piece
+        {D_COLOR, B_COLOR}, // DB piece
+        {F_COLOR, R_COLOR}, // FR piece
+        {F_COLOR, L_COLOR}, // FL piece
+        {B_COLOR, L_COLOR}, // BL piece
+        {B_COLOR, R_COLOR}  // BR piece
+    };
 };
--- src/cubie_cube.cpp
+++ src/cubie_cube.cpp
@@ -45,74 +45,22 @@ FaceletCube CubieCube::to_facelet_cube() const
     fc[4 + 9 * 4] = L_COLOR;
     fc[4 + 9 * 5] = B_COLOR;
 
-    static const int corner_facelets[8][3] = {
-        {8, 9, 20},   // URF slot: U, R, F facelets
-        {6, 18, 38},  // UFL slot: U, F, L facelets
-        {0, 36, 47},  // ULB slot: U, L, B facelets
-        {2, 45, 11},  // UBR slot: U, B, R facelets
-        {29, 26, 15}, // DFR slot: D, F, R facelets
-        {27, 44, 24}, // DFL slot: D, L, F facelets
-        {33, 53, 42}, // DLB slot: D, B, L facelets
-        {35, 17, 51}  // DBR slot: D, R, B facelets
-    };
-
-    static const Color corner_colors[8][3] = {
-        {U_COLOR, R_COLOR, F_COLOR}, // URF piece
-        {U_COLOR, F_COLOR, L_COLOR}, // UFL piece
-        {U_COLOR, L_COLOR, B_COLOR}, // ULB piece
-        {U_COLOR, B_COLOR, R_COLOR}, // UBR piece
-        {D_COLOR, F_COLOR, R_COLOR}, // DFR piece
-        {D_COLOR, L_COLOR, F_COLOR}, // DFL piece
-        {D_COLOR, B_COLOR, L_COLOR}, // DLB piece
-        {D_COLOR, R_COLOR, B_COLOR}  // DBR piece
-    };
-
     for (int i = 0; i < CORNER_COUNT; ++i)
     {
         int piece = static_cast<int>(cp[i]);
         int orient = co[i];
         for (int n = 0; n < 3; ++n)
         {
-            fc[corner_facelets[i][(n + orient) % 3]] = corner_colors[piece][n];
+            fc[CORNER_FACELETS[i][(n + orient) % 3]] = CORNER_COLORS[piece][n];
         }
     }
-
-    static const int edge_facelets[12][2] = {
-        {5, 10},  // UR slot: U6, R2
-        {7, 19},  // UF slot: U8, F2
-        {3, 37},  // UL slot: U4, L2
-        {1, 46},  // UB slot: U2, B2
-        {32, 16}, // DR slot: D6, R8
-        {28, 25}, // DF slot: D2, F8
-        {30, 43}, // DL slot: D4, L8
-        {34, 52}, // DB slot: D8, B8
-        {23, 12}, // FR slot: F6, R4
-        {21, 41}, // FL slot: F4, L6
-        {50, 39}, // BL slot: B6, L4
-        {48, 14}  // BR slot: B4, R6
-    };
-
-    static const Color edge_colors[12][2] = {
-        {U_COLOR, R_COLOR}, // UR piece
-        {U_COLOR, F_COLOR}, // UF piece
-        {U_COLOR, L_COLOR}, // UL piece
-        {U_COLOR, B_COLOR}, // UB piece
-        {D_COLOR, R_COLOR}, // DR piece
-        {D_COLOR, F_COLOR}, // DF piece
-        {D_COLOR, L_COLOR}, // DL piece
-        {D_COLOR, B_COLOR}, // DB piece
-        {F_COLOR, R_COLOR}, // FR piece
-        {F_COLOR, L_COLOR}, // FL piece
-        {B_COLOR, L_COLOR}, // BL piece
-        {B_COLOR, R_COLOR}  // BR piece
-    };
 
     for (int i = 0; i < EDGE_COUNT; ++i)
     {
         int piece = static_cast<int>(ep[i]);
         int orient = eo[i];
-        fc[edge_facelets[i][orient]] = edge_colors[piece][0];
-        fc[edge_facelets[i][1 - orient]] = edge_colors[piece][1];
+        fc[EDGE_FACELETS[i][orient]] = EDGE_COLORS[piece][0];
+        fc[EDGE_FACELETS[i][1 - orient]] = EDGE_COLORS[piece][1];
     }
 
     return fc;
@@ -340,4 +288,84 @@ int CubieCube::get_ud_slice_perm_coordinate() const
         factorial /= (3 - i);
     }
     return index;
+}
+
+bool CubieCube::is_solvable() const {
+    // sum check of co
+    int sum = 0;
+    for (int i = 0; i < CORNER_COUNT; i++) {
+        sum += co[i];
+    }
+    if (sum % 3 != 0)
+        return false;
+
+    // sum check of eo
+    sum = 0;
+    for (int i = 0; i < EDGE_COUNT; i++) {
+        sum += eo[i];
+    }
+    if (sum % 2 != 0)
+        return false;
+    
+    int ep_inv = 0;
+    for (int i = 0; i < EDGE_COUNT - 1; i++) {
+        for (int j = i + 1; j < EDGE_COUNT; j++) {
+            if (ep[i] > ep[j]) ep_inv++;
+        }
+    }
+
+    int cp_inv = 0;
+    for (int i = 0; i < CORNER_COUNT - 1; i++) {
+        for (int j = i + 1; j < CORNER_COUNT; j++) {
+            if (cp[i] > cp[j]) cp_inv++;
+        }
+    }
+
+    // edge and corner parity must match
+    return (ep_inv % 2) == (cp_inv % 2);
+}
+
+void CubieCube::from_facelet_cube(const FaceletCube& fc) {
+    for (int i = 0; i < CORNER_COUNT; ++i) {
+        int ori = -1;
+        for (int x = 0; x < 3; ++x) {
+            Color c = fc[CORNER_FACELETS[i][x]];
+            if (c == U_COLOR || c == D_COLOR) {
+                ori = x;
+                break;
+            }
+        }
+        co[i] = ori;
+
+        Color c0 = fc[CORNER_FACELETS[i][ori]];
+        Color c1 = fc[CORNER_FACELETS[i][(1 + ori) % 3]];
+        Color c2 = fc[CORNER_FACELETS[i][(2 + ori) % 3]];
+
+        for (int p = 0; p < CORNER_COUNT; ++p) {
+            if (CORNER_COLORS[p][0] == c0 &&
+                CORNER_COLORS[p][1] == c1 &&
+                CORNER_COLORS[p][2] == c2) {
+                cp[i] = static_cast<Corner>(p);
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < EDGE_COUNT; ++i) {
+        Color c0 = fc[EDGE_FACELETS[i][0]];
+        Color c1 = fc[EDGE_FACELETS[i][1]];
+
+        for (int p = 0; p < EDGE_COUNT; ++p) {
+            if (EDGE_COLORS[p][0] == c0 && EDGE_COLORS[p][1] == c1) {
+                ep[i] = static_cast<Edge>(p);
+                eo[i] = 0;
+                break;
+            }
+            if (EDGE_COLORS[p][0] == c1 && EDGE_COLORS[p][1] == c0) {
+                ep[i] = static_cast<Edge>(p);
+                eo[i] = 1;
+                break;
+            }
+        }
+    }
 }
